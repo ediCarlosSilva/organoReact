b3cded7f7cebe016795f6256d714144b5c911fe4
Edi Carlos <edi_csilva@hotmail.com>
2023-04-12 16:40:21 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
+import Rodape from './componentes/Rodape';
 
 function App() {
 
@@ -61,9 +62,10 @@ function App() {
           nome={time.nome}
           corPrimaria={time.corPrimaria}
           corSecundaria={time.corSecundaria}
-          colaboradores={colaboradores}
+          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
         />)
       }
+      <Rodape />
     </div>
   );
 }
